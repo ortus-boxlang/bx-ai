d3449ff721174d1a652feb1c9038e9ea88ef517e
Luis Majano <lmajano@gmail.com>
2025-03-12 10:26:11 +0100

    disable test until jacob finishes

--- src/test/java/ortus/boxlang/moduleslug/IntegrationTest.java
+++ src/test/java/ortus/boxlang/moduleslug/IntegrationTest.java
@@ -19,6 +19,7 @@ import static com.google.common.truth.Truth.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
