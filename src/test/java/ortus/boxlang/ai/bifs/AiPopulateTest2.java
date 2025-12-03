5c2961b43f4b4ba0a6b77e63395ac1b3a8077ca5
Luis Majano <lmajano@gmail.com>
2025-12-03 22:58:10 +0100

    small text chanage

--- src/test/java/ortus/boxlang/ai/bifs/AiPopulateTest2.java
+++ src/test/java/ortus/boxlang/ai/bifs/AiPopulateTest2.java
@@ -28,7 +28,7 @@ import ortus.boxlang.runtime.scopes.Key;
 /**
  * Tests for aiPopulate() BIF
  */
-public class AiPopulateTest extends BaseIntegrationTest {
+public class AiPopulateTest2 extends BaseIntegrationTest {
 
 	@Test
 	@DisplayName( "Can populate a class instance from JSON string" )
