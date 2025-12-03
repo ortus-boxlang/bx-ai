aabfbbd81396ddc5771a129ad9d2ecdeefac5e96
Luis Majano <lmajano@gmail.com>
2025-12-03 23:01:02 +0100

    package changes

--- src/test/java/ortus/boxlang/ai/mcp/mcpClientTest.java
+++ src/test/java/ortus/boxlang/ai/mcp/mcpClientTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ortus.boxlang.ai.Mcp;
+package ortus.boxlang.ai.mcp;
 
 import static com.google.common.truth.Truth.assertThat;
 
